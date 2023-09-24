f6f39979e302fa7e25bf669681d529f4458d7ceb
grahit27 <grahit27@gmail.com>
2023-09-24 16:22:41 +0530

    Updates

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Body from "./src/component/Body";
-const AppLayout = () => {
-  return <Body />;
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
